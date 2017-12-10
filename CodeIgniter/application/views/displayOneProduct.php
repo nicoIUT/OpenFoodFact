70fb15605ea2ee5809f0cf1a0504841349df2745
cellierantoine <cellier.antoine@gmail.com>
2017-12-10 18:12:46 +0100

    ajout de bootstrap et CSS de displayOneProduct

--- CodeIgniter/application/views/displayOneProduct.php
+++ CodeIgniter/application/views/displayOneProduct.php
@@ -1,177 +1,179 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-function ingredientTree($ingredient, $tree, $indent){
+function ingredientTree($ingredient, $tree){
+	echo $ingredient.'<br/>';
 	if(!array_key_exists($ingredient, $tree)){
 		return;
 	}
+	echo "<ul>";
 	foreach ($tree[$ingredient] as $subIngredient){
-		echo $indent.$subIngredient.'<br/>';
-		ingredientTree($subIngredient, $tree, $indent.'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
+		echo '<li>';
+		ingredientTree($subIngredient, $tree);
+		echo '</li>';
 	}
+	echo "</ul>";
 }
 
 
 ?>
 
-<?php echo print_r($product['firstRankIngredient']) ?>
-<br><br>
-<?php echo print_r($product['ingredient']) ?>
-<br><br>
-<?php echo print_r($product) ?>
-<br><br>
-<?php echo print_r($product['additif']) ?>
+<h1 class="display-3 mb-5"><?php echo $product['product']['product_name']?></h1>
 
-
-
-<h1><?php echo $product['product']['product_name']?></h1>
-<h2>caracteristiques :</h2>
-<table>
-	<tr>
-		<th>code : </th>
-		<td><?php echo $product['product']['id_produit'] ?></td>
-	</tr>
-	<tr>
-		<th>marque : </th>
-		<td><?php echo $product['product']['brands'] ?></td>
-	</tr>
-	<tr>
-		<th>portion : </th>
-		<td><?php echo $product['product']['serving_size'] ?></td>
-	</tr>
-	<tr>
-		<th>pays : </th>
-		<td><?php echo $product['product']['countries_fr'] ?></td>
-	</tr>
-</table>
-
-<h2>Additifs :</h2>
-<?php if(!empty($product['additif'])) :?>
-	<table>
-		<tr>
-			<th>code</th>
-			<th>nom</th>
-		</tr>
-
-		<?php foreach($product['additif'] as $additif) : ?>
+<div class="row">
+	<div class="col">
+		<h2>Caracteristiques</h2>
+		<table class="table table-sm">
 			<tr>
-				<td><?php echo $additif['id_additif'] ?></td>
-				<td><?php echo $additif['nom'] ?></td>
+				<th>Code</th>
+				<td><?php echo $product['product']['id_produit'] ?></td>
 			</tr>
-		<?php endforeach ?>
-	</table>
-<?php else : ?>
-	<p>Aucun additifs renseignés</p>
-<?php endif ?>
+			<tr>
+				<th>Marque</th>
+				<td><?php echo $product['product']['brands'] ?></td>
+			</tr>
+			<tr>
+				<th>Portion</th>
+				<td><?php echo $product['product']['serving_size'] ?></td>
+			</tr>
+			<tr>
+				<th>Pays</th>
+				<td><?php echo $product['product']['countries_fr'] ?></td>
+			</tr>
+		</table>
 
-<h2>Ingredients :</h2>
-<?php foreach($product['firstRankIngredient'] as $ingredient){
-	echo $ingredient.'<br/>';
-	ingredientTree($ingredient, $product['ingredient'], '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
-}
-?>
+		<h2>Nutri-score</h2>
+		<?php switch($product['product']['nutrition_grade_fr']){
+			case 'a':
+				echo "<img src=\"".base_url()."assets/image/nutriscore/A.png\" alt='nutriscore A'/>";
+				break;
+			case 'b':
+				echo "<img src=\"".base_url()."assets/image/nutriscore/B.png\" alt='nutriscore B'/>";
+				break;
+			case 'c':
+				echo "<img src=\"".base_url()."assets/image/nutriscore/C.png\" alt='nutriscore C'/>";
+				break;
+			case 'd':
+				echo "<img src=\"".base_url()."assets/image/nutriscore/D.png\" alt='nutriscore D'/>";
+				break;
+			case 'e':
+				echo "<img src=\"".base_url()."assets/image/nutriscore/E.png\" alt='nutriscore E'/>";
+				break;
+			default:
+				echo "<p>Nutri-score non renseigné</p>";
+		} ?>
 
-<h2>Nutrition :</h2>
-<table>
-	<tr>
-		<th>Valeur moyenne pour :</th>
-		<th>100g</th>
-	</tr>
-	<tr>
-		<td>energie</td>
-		<td><?php echo $product['product']['energy_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>graisse</td>
-		<td><?php echo $product['product']['fat_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>graisse saturée</td>
-		<td><?php echo $product['product']['satured_fat_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>graisse trans</td>
-		<td><?php echo $product['product']['trans_fat_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>cholesterol</td>
-		<td><?php echo $product['product']['cholesterol_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>carbohydrates</td>
-		<td><?php echo $product['product']['carbohydrates_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>sucres</td>
-		<td><?php echo $product['product']['sugars_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>fibres</td>
-		<td><?php echo $product['product']['fibers_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>protéines</td>
-		<td><?php echo $product['product']['proteins_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>sel</td>
-		<td><?php echo $product['product']['salt_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>sodium</td>
-		<td><?php echo $product['product']['sodium_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>vitamine A</td>
-		<td><?php echo $product['product']['vitamin_a_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>vitamine C</td>
-		<td><?php echo $product['product']['vitamin_c_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>calcium</td>
-		<td><?php echo $product['product']['calcium_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>fer</td>
-		<td><?php echo $product['product']['iron_100g'] ?></td>
-	</tr>
-	<tr>
-		<td>Score nutritif</td>
-		<td><?php echo $product['product']['nutrition_score_fr_100g'] ?></td>
-	</tr>
-</table>
+		<h2>Additifs</h2>
+		<?php if(!empty($product['additif'])) :?>
+			<table class="table table-striped table-sm">
+				<tr>
+					<th>Code</th>
+					<th>Nom</th>
+				</tr>
 
-<h2>Nutri-score :</h2>
-<?php switch($product['product']['nutrition_grade_fr']){
-	case 'a':
-		echo "<img src=".base_url().'assets/image/nutriscore/A.png'." alt='nutriscore A'>";
-		break;
-	case 'b':
-		echo "<img src=".base_url().'assets/image/nutriscore/B.png'." alt='nutriscore B'>";
-		break;
-	case 'c':
-		echo "<img src=".base_url().'assets/image/nutriscore/C.png'." alt='nutriscore C'>";
-		break;
-	case 'd':
-		echo "<img src=".base_url().'assets/image/nutriscore/D.png'." alt='nutriscore D'>";
-		break;
-	case 'e':
-		echo "<img src=".base_url().'assets/image/nutriscore/E.png'." alt='nutriscore E'>";
-		break;
-	default:
-		echo "<p>Nutri-score non renseigné</p>";
-} ?>
+				<?php foreach($product['additif'] as $additif) : ?>
+					<tr>
+						<td><?php echo $additif['id_additif'] ?></td>
+						<td><?php echo $additif['nom'] ?></td>
+					</tr>
+				<?php endforeach ?>
+			</table>
+		<?php else : ?>
+			<p>Aucun additifs renseignés</p>
+		<?php endif ?>
+
+		<h2>Ingredients</h2>
+		<?php if(!empty($product['ingredient'])){
+			foreach($product['firstRankIngredient'] as $ingredient){
+				ingredientTree($ingredient, $product['ingredient']);
+			}
+		}else{
+			echo "<p>Aucun ingrédients renseignés</p>";
+		}
+		?>
+	</div>
+	<div class="col">
+		<h2>Nutrition</h2>
+		<table class="table table-striped table-sm">
+			<tr>
+				<th>Valeur moyenne pour</th>
+				<th>100g</th>
+			</tr>
+			<tr>
+				<td>Energie</td>
+				<td><?php echo $product['product']['energy_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Graisse</td>
+				<td><?php echo $product['product']['fat_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Graisse saturée</td>
+				<td><?php echo $product['product']['satured_fat_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Graisse trans</td>
+				<td><?php echo $product['product']['trans_fat_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Cholesterol</td>
+				<td><?php echo $product['product']['cholesterol_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Carbohydrates</td>
+				<td><?php echo $product['product']['carbohydrates_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Sucres</td>
+				<td><?php echo $product['product']['sugars_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Fibres</td>
+				<td><?php echo $product['product']['fibers_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Protéines</td>
+				<td><?php echo $product['product']['proteins_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Sel</td>
+				<td><?php echo $product['product']['salt_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Sodium</td>
+				<td><?php echo $product['product']['sodium_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Vitamine A</td>
+				<td><?php echo $product['product']['vitamin_a_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Vitamine C</td>
+				<td><?php echo $product['product']['vitamin_c_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Calcium</td>
+				<td><?php echo $product['product']['calcium_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Fer</td>
+				<td><?php echo $product['product']['iron_100g'] ?></td>
+			</tr>
+			<tr>
+				<td>Score nutritif</td>
+				<td><?php echo $product['product']['nutrition_score_fr_100g'] ?></td>
+			</tr>
+		</table>
+	</div>
+</div>
 
 
-
-<h2>Informations complementaires :</h2>
-<table>
+<h2>Informations complementaires</h2>
+<table class="table table-sm">
 		<?php if(empty($product['product']['contributeur'])){
 			//Dans le cas d'une importation depuis un site tierce
 			echo "</tr>";
-			echo "<th>Importé depuis :</th>";
+			echo "<th>Importé depuis</th>";
 			echo "<td><a href=".$product['reference']['url'].">".$product['reference']['nom']."</a></td>";
 			echo "</tr>";
 		}else{
@@ -181,11 +183,11 @@ function ingredientTree($ingredient, $tree, $indent){
 		?>
 
 	<tr>
-		<th>Date de création :</th>
+		<th>Date de création</th>
 		<td><?php echo substr($product['product']['created_t'],0 ,19); ?></td>
 	</tr>
 	<tr>
-		<th>Date de derniere modification :</th>
+		<th>Date de derniere modification</th>
 		<td><?php echo substr($product['product']['last_modified_t'],0 ,19); ?></td>
 	</tr>
 
--- CodeIgniter/application/views/template.php
+++ CodeIgniter/application/views/template.php
@@ -6,9 +6,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html>
     <head>
         <title><?php echo $title; ?></title>
+        <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
     </head>
 
     <body>
-        <?php $this->load->view($content); ?>
+        <nav class="navbar bg-light">
+        <!--@TODO Changer le # pour l'adresse de la liste de produit-->
+            <a class="navbar-brand" href="#">OpenFoodFacts</a>
+        </nav>
+
+        <div class="container">
+            <?php $this->load->view($content); ?>
+        </div>
     </body>
 </html>
