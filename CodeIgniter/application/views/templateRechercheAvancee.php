9a4e6e92180966f80a7773d3af1c8d8acf4e439d
Antoine Cellier <cellier.antoine@gmail.com>
2017-12-12 21:40:53 +0100

    Modifs d'affichage ..................................

---
+++ CodeIgniter/application/views/templateRechercheAvancee.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$this->load->helper('form');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+        <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    </head>
+
+    <body>
+        <nav class="navbar bg-light">
+            <a class="navbar-brand" href='<?php echo base_url()."index.php/Produits/listProduct/" ?>'>OpenFoodFacts</a>
+        </nav>
+
+        <div class="container">
+            <?php $this->load->view($content); ?>
+        </div>
+    </body>
+</html>
