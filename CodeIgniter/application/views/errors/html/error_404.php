660058dad26ed7bc5ee82e82da27ef69ec375b97
Antoine Cellier <cellier.antoine@gmail.com>
2017-12-12 18:25:44 +0100

    bon il manquait quelque chose :D

--- CodeIgniter/application/views/errors/html/error_404.php
+++ CodeIgniter/application/views/errors/html/error_404.php
@@ -63,7 +63,7 @@ p {
 <body>
 	<div id="container">
 		<h1><?php echo "404 - Page introuvable"; ?></h1>
-        <div id ="image"><?php echo "<img class=\"img-responsive\" src=\"".base_url()."assets/image/404/error404.png\" alt='ERREUR 404'/>"; ?></div>
+        <div id ="image"><?php echo "<img class=\"img-responsive\" src=\"".base_url()."assets/image/erreur404/error404.png\" alt='ERREUR 404'/>"; ?></div>
 		<?php echo "</br> La page demandée n'a pas pu être trouvée"; ?>
 	</div>
 </body>
